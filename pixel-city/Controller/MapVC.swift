b03a4bb5814cf5f42f3d82fa9e91131efb1e179d
Artur Zarzecki <zarzecki.art@icloud.com>
2021-02-02 15:56:33 +0100

    CreatingXcodeProject Init commit

---
+++ pixel-city/Controller/MapVC.swift
@@ -0,0 +1,20 @@
+//
+//  MapVC.swift
+//  pixel-city
+//
+//  Created by Artur Zarzecki on 02/02/2021.
+//  Copyright © 2021 Artur Zarzecki. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
