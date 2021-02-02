57841abf7abc25c49645f41d13d0523b0ffef5f7
Artur Zarzecki <zarzecki.art@icloud.com>
2021-02-02 20:42:09 +0100

    Let usee allow to use location. Zoom in map on current location, make btn to come back to current location

--- pixel-city/Controller/MapVC.swift
+++ pixel-city/Controller/MapVC.swift
@@ -8,24 +8,54 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapVC: UIViewController {
     
     @IBOutlet weak var mapView: MKMapView!
     
+    var locationManager = CLLocationManager()
+    let authorizationStatus = CLLocationManager.authorizationStatus() //keep tracking our authorization status
+    let regionRedius: Double = 1000 // region is 1000 meter large
 
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
         
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        configureLocetionServices()
     }
 
     @IBAction func centerMapBtnWasPressed(_ sender: Any) {
+        if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse{
+            centerMapOnUserLocation()
+        }
     }
     
 }
 
 extension MapVC: MKMapViewDelegate {
-    
+    func centerMapOnUserLocation() {
+        guard let coordinate = locationManager.location?.coordinate else { return }
+        let coordinateRegion = MKCoordinateRegion.init(center: coordinate, latitudinalMeters: regionRedius * 2.0, longitudinalMeters: regionRedius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
 }
 
+extension MapVC: CLLocationManagerDelegate {
+    func configureLocetionServices() {
+        //check if we are authorize to use our curent location
+        if authorizationStatus == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        } else {
+            //if we are already approved or deneid there is no need to do any hangouts
+            return
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) { // is called evry time the map is calling authorization
+        centerMapOnUserLocation()
+    }
+}
